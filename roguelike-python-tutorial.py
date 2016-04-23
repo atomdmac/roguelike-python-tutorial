c79473d88b0fd2d0fbe9fde9f790082e2027a21d
suicidepills <atom.mac@gmail.com>
2016-04-23 00:49:29 -0400

    Add dungeon generation!
    Signed-off-by: suicidepills <atom.mac@gmail.com>

--- roguelike-python-tutorial.py
+++ roguelike-python-tutorial.py
@@ -40,7 +40,9 @@ libtcod.sys_set_fps(LIMIT_FPS)
 # Initialize minor consoles
 con = libtcod.console_new(SCREEN_WIDTH, SCREEN_HEIGHT)
 
+################################################################################
 # Object Class!
+################################################################################
 class Object:
     #this is a generic object: the player, a monster, an item, the stairs...
     #it's always represented by a character on screen.
@@ -65,6 +67,62 @@ class Object:
         #erase the character that represents this object
         libtcod.console_put_char(con, self.x, self.y, ' ', libtcod.BKGND_NONE)
 
+################################################################################
+# Initialize actors and items
+################################################################################
+player = Object(SCREEN_WIDTH/2, SCREEN_HEIGHT/2, '@', libtcod.white)
+npc = Object(SCREEN_WIDTH/2 - 5, SCREEN_HEIGHT/2, '@', libtcod.yellow)
+objects = [npc, player]
+
+# !! DEBUG !! Place player in debug room
+player.x = 25
+player.y = 22
+
+################################################################################
+# Rectangles and Rooms
+################################################################################
+ROOM_MAX_SIZE = 10
+ROOM_MIN_SIZE = 6
+MAX_ROOMS = 30
+
+class Rect:
+    #a rectangle on the map. used to characterize a room.
+    def __init__(self, x, y, w, h):
+        self.x1 = x
+        self.y1 = y
+        self.x2 = x + w
+        self.y2 = y + h
+
+    def center(self):
+        center_x = (self.x1 + self.x2) / 2
+        center_y = (self.y1 + self.y2) / 2
+        return (center_x, center_y)
+ 
+    def intersect(self, other):
+        #returns true if this rectangle intersects with another one
+        return (self.x1 <= other.x2 and self.x2 >= other.x1 and
+                self.y1 <= other.y2 and self.y2 >= other.y1)
+
+def create_room(room):
+    global map
+    #go through the tiles in the rectangle and make them passable
+    for x in range(room.x1 + 1, room.x2):
+        for y in range(room.y1 + 1, room.y2):
+            map[x][y].blocked = False
+            map[x][y].block_sight = False
+
+def create_h_tunnel(x1, x2, y):
+    global map
+    for x in range(min(x1, x2), max(x1, x2) + 1):
+        map[x][y].blocked = False
+        map[x][y].block_sight = False
+
+def create_v_tunnel(y1, y2, x):
+    global map
+    #vertical tunnel
+    for y in range(min(y1, y2), max(y1, y2) + 1):
+        map[x][y].blocked = False
+        map[x][y].block_sight = False
 
 ################################################################################
 # Map
@@ -88,14 +146,65 @@ def make_map():
     global map
  
     # Fill map with "unblocked" tiles
-    map = [[ Tile(False)
+    map = [[ Tile(True)
         for y in range(MAP_HEIGHT) ]
             for x in range(MAP_WIDTH) ]
 
-    map[30][22].blocked = True
-    map[30][22].block_sight = True
-    map[50][22].blocked = True
-    map[50][22].block_sight = True
+    rooms = []
+    num_rooms = 0
+
+    for r in range(MAX_ROOMS):
+        #random width and height
+        w = libtcod.random_get_int(0, ROOM_MIN_SIZE, ROOM_MAX_SIZE)
+        h = libtcod.random_get_int(0, ROOM_MIN_SIZE, ROOM_MAX_SIZE)
+        #random position without going out of the boundaries of the map
+        x = libtcod.random_get_int(0, 0, MAP_WIDTH - w - 1)
+        y = libtcod.random_get_int(0, 0, MAP_HEIGHT - h - 1)
+
+        #"Rect" class makes rectangles easier to work with
+        new_room = Rect(x, y, w, h)
+ 
+        #run through the other rooms and see if they intersect with this one
+        failed = False
+        for other_room in rooms:
+            if new_room.intersect(other_room):
+                failed = True
+                break
+
+        if not failed:
+            #this means there are no intersections, so this room is valid
+ 
+            #"paint" it to the map's tiles
+            create_room(new_room)
+ 
+            #center coordinates of new room, will be useful later
+            (new_x, new_y) = new_room.center()
+ 
+            if num_rooms == 0:
+                #this is the first room, where the player starts at
+                player.x = new_x
+                player.y = new_y
+                
+            else:
+                #all rooms after the first:
+                #connect it to the previous room with a tunnel
+ 
+                #center coordinates of previous room
+                (prev_x, prev_y) = rooms[num_rooms-1].center()
+ 
+                #draw a coin (random number that is either 0 or 1)
+                if libtcod.random_get_int(0, 0, 1) == 1:
+                    #first move horizontally, then vertically
+                    create_h_tunnel(prev_x, new_x, prev_y)
+                    create_v_tunnel(prev_y, new_y, new_x)
+                else:
+                    #first move vertically, then horizontally
+                    create_v_tunnel(prev_y, new_y, prev_x)
+                    create_h_tunnel(prev_x, new_x, new_y)
+ 
+            #finally, append the new room to the list
+            rooms.append(new_room)
+            num_rooms += 1
 
 def render_all():
     #draw all objects in the list
@@ -112,17 +221,11 @@ def render_all():
 
     libtcod.console_blit(con, 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT, 0, 0, 0)
 
+
 ################################################################################
 # Initialize the map
 ################################################################################
 make_map()
-
-################################################################################
-# Initialize actors and items
-################################################################################
-player = Object(SCREEN_WIDTH/2, SCREEN_HEIGHT/2, '@', libtcod.white)
-npc = Object(SCREEN_WIDTH/2 - 5, SCREEN_HEIGHT/2, '@', libtcod.yellow)
-objects = [npc, player]
 
 ################################################################################
 # Event Loop
